f6b3fcf2904c20f33b73d77a6a53499e948509da
olesya-arch <less2626@rambler.ru>
2021-04-05 19:49:53 +0300

    homework 2,1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,19 +4,18 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		int yearOfBirthday = 1982;
 		int monthOfBirthday = 12;
 		int dayOfBirthday = 26;
 		int sum = yearOfBirthday + monthOfBirthday + dayOfBirthday;
 		System.out.println("Сумма моего года, месяца и дня рождения:" + sum);
-		boolean monthBiggerDay = false;
-		while (!monthBiggerDay) {
-			monthBiggerDay = true;
-			if (monthOfBirthday > dayOfBirthday) {
-				System.out.println("Месяц моего рождения больше даты моего рождения:" + true);
-			} else System.out.println("Месяц моего рождения больше даты моего рождения:" + false);
-		}
+
+		if (monthOfBirthday > dayOfBirthday) {
+			System.out.println("Месяц моего рождения больше даты моего рождения:" + true);
+		} else System.out.println("Месяц моего рождения больше даты моего рождения:" + false);
+
+
 
 		char [] a = new char[5];
 		a [0] = 'O';
@@ -24,7 +23,8 @@ public class Main {
 		a [2] = 'е';
 		a [3] = 'с';
 		a [4] = 'я';
-		System.out.print("Массив с моим именем: "); System.out.println(a);
+		System.out.print("Массив с моим именем: ");
+		System.out.println(a);
 
 		double myAge = 38;
 		double partYear = 3.00 / 12.00;
